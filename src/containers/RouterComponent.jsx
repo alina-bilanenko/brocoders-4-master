f44fccdcd057ad33e107720fc7c485fc281b8e1d
alina-bilanenko <Linyska007@gmail.com>
2019-09-26 16:11:15 +0300

    first commit

--- src/containers/RouterComponent.jsx
+++ src/containers/RouterComponent.jsx
@@ -4,7 +4,7 @@ import TaskTimer from "containers/TaskTimer";
 import Info from "componentns/Tasks/Info";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 
 const RouterComponent = ({history}) => {
@@ -15,6 +15,7 @@ const RouterComponent = ({history}) => {
         <Route exact path="/taskChart" component={TaskTimer} />
         <Route path="/task/:id" component={Info} />
         <Route component={NotFound} />
+        <Redirect exact path="/" />
       </Switch>
     </ConnectedRouter>
   );
