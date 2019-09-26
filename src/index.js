0b36854b57d009d30b76b8a0e863af1b3a9fa2cf
alina-bilanenko <Linyska007@gmail.com>
2019-09-26 16:52:15 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { homepage } from '../package.json'
 const sagaMiddleware = createSagaMiddleware();
 
 const history = createBrowserHistory({
-  basename: 'src'
+  basename: '/'
 });
 function configureStore() {
   const store = createStore(
